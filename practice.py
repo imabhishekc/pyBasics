ff71914734c5da8356011514263b19d4a41c8424
Abhishek Chakraborty <85833164+imabhishekc@users.noreply.github.com>
2023-12-22 10:52:12 +0545

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,74 +1,74 @@
-# i = int(input("Enter your first number: "))
-# operator = input("Enter your operator: ")
-# k = int(input("Enter your second number: "))
+ i = int(input("Enter your first number: "))
+ operator = input("Enter your operator: ")
+ k = int(input("Enter your second number: "))
 
-# if operator == '+':
-#     print(i + k)
-# elif operator == '-':
-#     print(i - k)
-# elif operator == '*':
-#     print(i * k)
-# elif operator == '/':
-#     print(i/k)
-# elif operator == '^':
-#     print(i**k)
-# elif operator == '%':
-#     print(i%k)
-# else:
-#     print("Invalid operator")                        
+ if operator == '+':
+     print(i + k)
+ elif operator == '-':
+     print(i - k)
+ elif operator == '*':
+     print(i * k)
+ elif operator == '/':
+     print(i/k)
+ elif operator == '^':
+     print(i**k)
+ elif operator == '%':
+     print(i%k)
+ else:
+     print("Invalid operator")                        
 
-# my_name = 'Zed A. Shaw'
-# my_age = 35 # not a lie
-# my_height = 74 # inches
-# my_weight = 180 # lbs
-# my_eyes = 'Blue'
-# my_teeth = 'White'
-# my_hair = 'Brown'
+ my_name = 'Zed A. Shaw'
+ my_age = 35 # not a lie
+ my_height = 74 # inches
+ my_weight = 180 # lbs
+ my_eyes = 'Blue'
+ my_teeth = 'White'
+ my_hair = 'Brown'
 
-# my_height_in_cm = my_height * 2.54
-# my_weight_in_kg = my_weight * 0.453592
+ my_height_in_cm = my_height * 2.54
+ my_weight_in_kg = my_weight * 0.453592
 
-# print ("Let's talk abloy %s" % my_name)
-# print("He's %d inches tall." % my_height)
-# print("He's %d pounds heavy" % my_weight)
-# print("actually that's not too heavy")
-# print("He's got %s eyes and %s hair."%(my_eyes, my_hair))
-# print("His teeth usually %s depending on the coffee."% my_teeth)
+ print ("Let's talk abloy %s" % my_name)
+ print("He's %d inches tall." % my_height)
+ print("He's %d pounds heavy" % my_weight)
+ print("actually that's not too heavy")
+ print("He's got %s eyes and %s hair."%(my_eyes, my_hair))
+ print("His teeth usually %s depending on the coffee."% my_teeth)
 
-# print("If I add %d, %d and %d I get %d" % (my_age, my_height, my_weight, my_age + my_height + my_weight))
+ print("If I add %d, %d and %d I get %d" % (my_age, my_height, my_weight, my_age + my_height + my_weight))
 
-# print("my height in centimeters is %d" %(my_height_in_cm))
-# print("my weight in kilograms is %d" %(my_weight_in_kg))
+ print("my height in centimeters is %d" %(my_height_in_cm))
+ print("my weight in kilograms is %d" %(my_weight_in_kg))
 
-# print(round(1.34232))
+ print(round(1.34232))
 
-# formatter = "%r %r %r %r"
+ formatter = "%r %r %r %r"
 
-# print (formatter % (1, 2, 3, 4))
-# print(formatter % ('one', 'two', 'three', 'four'))
-# print(formatter % (True, False, False, True))
-# print(formatter % (formatter, formatter, formatter, formatter))
-# print(formatter % (
-#     "I got this thing right",
-#     "I will make things better",
-#     "It didn't break",
-#     "I am not an ordinary person"
-# ))
+ print (formatter % (1, 2, 3, 4))
+ print(formatter % ('one', 'two', 'three', 'four'))
+ print(formatter % (True, False, False, True))
+ print(formatter % (formatter, formatter, formatter, formatter))
+ print(formatter % (
+     "I got this thing right",
+     "I will make things better",
+     "It didn't break",
+     "I am not an ordinary person"
+ ))
 
 
-# formatter = "%r"
-# print(formatter %'Hello There I\'ve been waiting for you')
+ formatter = "%r"
+ print(formatter %'Hello There I\'ve been waiting for you')
 
-# print("How old are you?"),
-# age = input()
-# print("How tall are you?"),
-# height = input()
-# print("How much do you weigh?"),
-# weight = input()
+ print("How old are you?"),
+ age = input()
+ print("How tall are you?"),
+ height = input()
+ print("How much do you weigh?"),
+ weight = input()
 
-# print("So you're %r old, %r tall and %r heavy" % (age, height, weight)),
+ print("So you're %r old, %r tall and %r heavy" % (age, height, weight)),
 
-# y = input("Name?")
+ y = input("Name?")
 
 age = input("What is your age?")
 height = input("What is your height?")
